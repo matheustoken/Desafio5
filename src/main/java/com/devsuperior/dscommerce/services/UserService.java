1b8be63257be27739c6f1d968bdeafe2d22cd12e
matheustoken <accorsimagno@gmail.com>
2024-12-18 21:14:54 -0300

    Implementacao UserDetaildsService

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -22,12 +22,12 @@ import com.devsuperior.dscommerce.repositories.UserRepository;
 public class UserService implements UserDetailsService {
 
 	@Autowired
-	private UserRepository repository;
+	private UserRepository userRepository;
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
-		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+		List<UserDetailsProjection> result = userRepository.searchUserAndRolesByEmail(username);
 		if (result.size() == 0) {
 			throw new UsernameNotFoundException("Email not found");
 		}
